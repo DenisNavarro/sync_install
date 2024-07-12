db679697c61823eb7a68e28ba3b867ef27a751bb
Denis Navarro <denis.navarro@mailfence.com>
2024-07-12 21:47:27 +0200

    test: Rename a constant in tests/cli.rs

--- tests/cli.rs
+++ tests/cli.rs
@@ -2,7 +2,7 @@ use anyhow::{ensure, Context};
 use camino::Utf8PathBuf;
 use cargo_metadata::MetadataCommand;
 
-const EXPECTED_RESULT: &str = r"This is a dry run. Add the --go option to execute the below command(s).
+const EXPECTED_OUTPUT: &str = r"This is a dry run. Add the --go option to execute the below command(s).
 ---> [cargo uninstall genact]
 ---> [cargo install cargo-cache --version 0.8.3 --force]
 ---> [cargo install xh --version 0.22.0 --locked]
@@ -19,7 +19,7 @@ fn example_from_the_cli_help_and_the_readme() -> anyhow::Result<()> {
     let status = output.status;
     ensure!(status.success(), "error status: {status}");
     let stdout = String::from_utf8(output.stdout).context("non-UTF8 command output")?;
-    assert_eq!(stdout, EXPECTED_RESULT);
+    assert_eq!(stdout, EXPECTED_OUTPUT);
     Ok(())
 }
 
