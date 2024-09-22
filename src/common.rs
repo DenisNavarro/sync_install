67cb569efcaea3827a6bbe4aae1c76fc95abf4e0
Denis Navarro <denis.navarro@mailfence.com>
2024-09-22 12:10:32 +0200

    refactor: Rename 2 parameters

--- src/common.rs
+++ src/common.rs
@@ -3,23 +3,23 @@ use std::{fmt::Display, path::Path};
 use serde_json::json;
 
 #[must_use]
-pub fn quote(arg: &str) -> impl Display + '_ {
+pub fn quote(string: &str) -> impl Display + '_ {
     // The Rust documentation says:
     //
     // > `Debug` implementations of types provided by the standard library (`std`, `core`, `alloc`,
     // > etc.) are not stable, and may also change with future Rust versions.
     //
     // This is why I use `format!("{}", quote(string))` instead of `format!("{string:?}")`.
-    json!(arg)
+    json!(string)
 }
 
 #[must_use]
-pub fn quote_path(arg: &Path) -> impl Display + '_ {
+pub fn quote_path(path: &Path) -> impl Display + '_ {
     // The Rust documentation says:
     //
     // > `Debug` implementations of types provided by the standard library (`std`, `core`, `alloc`,
     // > etc.) are not stable, and may also change with future Rust versions.
     //
     // This is why I use `format!("{}", quote_path(path))` instead of `format!("{path:?}")`.
-    json!(arg)
+    json!(path)
 }
