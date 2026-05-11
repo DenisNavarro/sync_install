e9727fa908188e33c545cd37d6beee1ba8e5229b
Denis Navarro <denis.navarro@mailfence.com>
2026-05-11 22:51:37 +0200

    chore: Upgrade versions in examples

--- src/parsing_error_tests.rs
+++ src/parsing_error_tests.rs
@@ -44,7 +44,7 @@ fn same_crate_in_a_previous_line() -> anyhow::Result<()> {
 fn pixi_global_install_without_expected_suffix() -> anyhow::Result<()> {
     parse_first_arg_and_check_error_contains(
         r"RUN set -eux; \
-            pixi global install git=2.51.2",
+            pixi global install git=2.54.0",
         [
             "failed to parse line 2: ",
             r#"line with "pixi global install " but which does not end with "; \""#,
@@ -76,7 +76,7 @@ fn pixi_global_install_without_equal() -> anyhow::Result<()> {
 fn pixi_global_install_with_empty_recipe() -> anyhow::Result<()> {
     parse_first_arg_and_check_error_contains(
         r"RUN set -eux; \
-            pixi global install =2.51.2; \
+            pixi global install =2.54.0; \
             pixi clean cache --yes",
         ["failed to parse line 2: ", "empty recipe"],
     )
@@ -86,12 +86,12 @@ fn pixi_global_install_with_empty_recipe() -> anyhow::Result<()> {
 fn same_recipe_in_a_previous_line() -> anyhow::Result<()> {
     parse_first_arg_and_check_error_contains(
         r"RUN set -eux; \
-            pixi global install git=2.49.0; \
             pixi global install git=2.51.2; \
+            pixi global install git=2.54.0; \
             pixi clean cache --yes",
         [
             "failed to parse line 3: ",
-            r#""git" recipe already installed in a previous line: it was git=2.49.0"#,
+            r#""git" recipe already installed in a previous line: it was git=2.51.2"#,
         ],
     )
 }
