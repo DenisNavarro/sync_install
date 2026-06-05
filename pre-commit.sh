46541c59da099acea716121f384c65bb35fc6a66
Denis Navarro <denis.navarro@mailfence.com>
2026-06-05 02:18:54 +0200

    ci: In the pre-commit hook, put Podman builds in `git diff` conditions

--- pre-commit.sh
+++ pre-commit.sh
@@ -7,12 +7,16 @@ cargo +1.95.0 fmt --all --check
 cargo +1.95.0 clippy --all-features --all-targets --locked --workspace -- -D warnings
 cargo +1.95.0 test --locked --workspace
 
-cd dockerfiles
-podman build -f current_state_from_readme -t sync_install_current_state_from_readme
-podman build -f target_state_from_readme -t sync_install_target_state_from_readme
-podman build -f tested_example_1 -t sync_install_tested_example_1
-podman build -f tested_example_2 -t sync_install_tested_example_2
+if ! git diff --cached --quiet -- dockerfiles; then (
+    cd dockerfiles
+    podman build -f current_state_from_readme -t sync_install_current_state_from_readme
+    podman build -f target_state_from_readme -t sync_install_target_state_from_readme
+    podman build -f tested_example_1 -t sync_install_tested_example_1
+    podman build -f tested_example_2 -t sync_install_tested_example_2
+    podman image prune -f
+) fi
 
-cd ../idempotent_setup
-bash verify_dockerfile_and_setup.bash
-podman image prune -f
+if ! git diff --cached --quiet -- idempotent_setup; then (
+    cd idempotent_setup
+    bash verify_dockerfile_and_setup.bash
+) fi
